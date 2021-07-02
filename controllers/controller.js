183ad770ff051f0be8170a01fc5ac12c82660772
SKZ <stefan.kraenzle@opitz-consulting.com>
2021-07-02 08:04:41 +0200

    Added username input to createPost Endpoint

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,43 +1,47 @@
-const Post = require("../models/Post");
+const Post = require('../models/Post')
 // const Post = db.Post;
 
 exports.create = (req, res) => {
-  console.log("Post created via REST");
+  console.log('Post created via REST')
   if (!req.body.body) {
-    res.status(400).send({ message: "Post must not be empty" });
-    return;
+    res.status(400).send({ message: 'Post must not be empty' })
+    return
+  }
+  if (!req.body.username) {
+    res.status(400).send({ message: 'Username must not be empty' })
+    return
   }
   const newPost = new Post({
-    body: req.body.body,
-    username: "mock username",
+    body: req.body.postBody,
+    username: req.body.username,
     createdAt: new Date().toISOString(),
-  });
+  })
 
   newPost
     .save(newPost)
     .then((data) => {
-      res.send(data);
+      res.send(data)
     })
     .catch((err) => {
       res.status(510).send({
-        message: err.message || "Error while saving post",
-      });
-    });
-  return newPost;
-};
+        message: err.message || 'Error while saving post',
+      })
+    })
+  return newPost
+}
 
 exports.findAll = (req, res) => {
   Post.find()
-    .sort({ createdAt: "desc" })
+    .sort({ createdAt: 'desc' })
     .then((data) => {
-      res.send(data);
+      res.send(data)
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || "Error while fetching posts",
-      });
-    });
-};
+        message: err.message || 'Error while fetching posts',
+      })
+    })
+}
 
 // exports.findOne = (req, res) => {};
 
@@ -48,25 +52,25 @@ exports.update = (req, res) => {
     { rawResult: true }
   )
     .then((data) => {
-      res.send(data);
+      res.send(data)
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || "Error while updating post " + req.body.id,
-      });
-    });
-};
+        message: err.message || 'Error while updating post ' + req.body.id,
+      })
+    })
+}
 
 exports.delete = (req, res) => {
   Post.findByIdAndDelete(req.params.id)
     .then(() => {
-      res.send("Post " + req.params.id + " deleted successfully");
+      res.send('Post ' + req.params.id + ' deleted successfully')
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || "Error while deleting post " + req.params.id,
-      });
-    });
-};
+        message: err.message || 'Error while deleting post ' + req.params.id,
+      })
+    })
+}
 
-exports.findAllPublished = (req, res) => {};
+exports.findAllPublished = (req, res) => {}
